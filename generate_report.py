3c38a7998d89146773bc5e85df7e0bbbb4532894
Jason Sung <jason870724@gmail.com>
2025-11-16 18:42:08 +0800

    add new product code

--- generate_report.py
+++ generate_report.py
@@ -340,4 +340,4 @@ class ReportGenerator:
 
 if __name__ == "__main__":
     generator = ReportGenerator()
-    generator.generate_report(input_data_path=r"C:\Users\07711.Jason.Sung\OneDrive - Global ICT\文件\1106.xls", output_path="123", platform="shopline")
+    generator.generate_report(input_data_path=r"/Users/jasonsung/Downloads/1116.xls", output_path="123", platform="shopline")
